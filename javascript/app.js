c10ed00f1bb4ffc07d6ad02f64bc16290752dd6e
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-14 16:16:51 -0500

    Commit 1

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+console.log("up and running");
+
+// 1. We assign value to a variable by declaring and defining a const variable -- const variable = "value", declaring and defining a let variable -- let variable = "value". With let variables, we can redefine and thus reassign the value by saying variable = "new value" 
+// 2. We can change the value of a variable through mutation. With let, we can reassign value by saying variable = "new variable"
+// 3. const existing = 123
+// 		const new = (existing*4). you can also use interpolation. For example:
+// 		const existing = "hello"
+// 		const new = (`Why, ${existing} there`)
+// 4. Declaring a variable doesn't necessarily give value to the variable, you're just saying it exists, i.e. let variable;.
+// 		Assigning value is the process of giving the variable a value using = "some value" or someNumber, etc. If we were to say let variable; we can later assign value by saying variable = 1.
+// 		Defining a variable is declaring the variable AND assigning value to it.	
+// 5. Pseudocoding is writing out, in plain English, what your code should be doing/what you want to be doing. It is extremely helpful for clarifying exactly what you want to do, what needs to be in your code, what your output should look like, etc.  It makes for more efficient, more effective, more accurate, cleaner programming
+// 6. You should spend about 75% of your time planning your program and about 25% actually coding your program.
+
