c9c5c9721b402249ad7ddbcc13c184383444809e
LehmanBrother <chrislehman1991@gmail.com>
2018-09-14 16:06:24 -0500

    q&a done

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//I. Variables and Datatypes
+	//A. Q + A
+		//1. How do we assign a value to a variable?
+			//let/const name = value;
+		//2. How do we change the value of a variable?
+			//Reassign it: name = value;
+		//3. How do we assign an existing variable to a new variable?
+			//newVar = oldVar;
+		//4. Remind me, what are declare, assign, and define?
+			//Declare: let/const var;
+			//Assign: var = value;
+			//Define: let/const var = value;
+		//5. What is pseudocoding and why should you do it?
+			//Pseudocoding consists of writing out what you want code to do in English to help plan the actual coding
+		//6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+			//Mostly thinking, little coding
