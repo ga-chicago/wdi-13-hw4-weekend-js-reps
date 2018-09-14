2ac50b203bdb84402e118ae0a90647b7ba85fd45
John <john@Johns-MacBook-Pro.local>
2018-09-14 17:52:05 -0500

    First Commit: Q & A

---
+++ js/app.js
@@ -0,0 +1,70 @@
+// Part I: Variables & Datatypes
+
+//	a. Q & A
+
+// 1. you assign value to a variable with an assignment operator - 
+// i.e. an equals sign = . Put a value across an assignment operator
+// from your variable, and it becomes the variable's value - 
+// and they become essentially interchangable. 
+
+// 2. you can't change the value of just any variable - 
+// const, for example, can't be redclared, but let can!
+// all you have to do is declare it afresh. 
+
+// 3. you reassign the value? I think that's what you're asking
+
+// 4. to declare a variable is to outline its structure and parameters 
+//  - but not necessarily to assign those parameters  meaning/value - 
+// within a given scope. 
+
+// to assign a variable is to give it a specific value.
+// This is done with an assignment operator, = . 
+
+// to define a variable is to assess or display its value. 
+
+// 5. psuedocoding is writing out, in language that's sort of 
+// half way between proper english and javascript syntax, 
+// a rough draft of your code. it's super useful for 
+// planning what you'll actually code, as you can break 
+// down steps and desired outcomes in a language that's 
+// more familiar (probably) to you than straight javascript.
+// 6. Hmm, I'm gonna say around 60 planning/40 coding?
+
+// b. Strings
+
+// 1. 
+
+let firstVariable = 'Hello world';
+
+firstVariable = 99; 
+
+let secondVariable = firstVariable;
+
+secondVariable = 'ibuprofin';
+
+// <-- ibuprofin
+
+// 2. 
+
+const yourName = 'John'
+
+console.log('Hello! My name is ' + yourName);
+
+//c. Booleans
+
+ const a = 4;
+ const b = 53;
+ const c = 57;
+ const d = 16;
+ const e = 'Kevin';
+
+ console.log(a < b);
+ console.log(c > d);
+ console.log('Name' === 'Name');
+ console.log(true !== false);
+ console.log(false === false === false === false === false === false === true); // ok, I don't understand why this is true but it is?
+ console.log(false === false);
+ console.log(e === 'Kevin');
+ console.log(a + b === c );
+ console.log(a * a === d);
+ console.log(48 == '48');
