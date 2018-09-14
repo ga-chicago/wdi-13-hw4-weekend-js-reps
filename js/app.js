838ab61453135dd4cb9fa1971801438fcc53e135
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-14 16:19:44 -0500

    variables and datatypes A

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log('HOMEWORK');
+
+
+//1. VARIABLES AND DATATYPES
+
+// How do we assign a value to a variable?
+
+//a. We assign a value to a variable with '='
+
+
+// How do we change the value of a variable?
+
+//a. By using the name of that same variable = to a different value.
+
+// How do we assign an existing variable to a new variable?
+
+// a. By declaring a new variable and making it equal to the name of the existing variable.
+
+// Remind me, what are declare, assign, and define?
+
+//a. when you declare a variable reserves the space for a variable to be used later.
+//b. when you are assigning a variable you are setting the value.
+//c. when you define a variable comprises both the declaration and assignment.
+
+// What is pseudocoding and why should you do it?
+
+//a. writing the code in english to understand the steps it takes to get things done properly! Helps to make a guidline.
+
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+// a. 75-90% planning and 10-25% actually code
