da2b281378904f17b6e12bebd83f01bc738a84bd
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-14 16:33:12 -0500

    booleans

--- js/app.js
+++ js/app.js
@@ -26,10 +26,31 @@ console.log("works");
 // What is the value of firstVariable?
 // Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
 
-let firstVariable = "Hello World";
- firstVariable = 44;
-let secondVariable = firstVariable;
-secondVariable = "any string";
-//the value of firstVariable is 44
-let yourName = "fil";
-console.log("Hello, my name is " + yourName + ".");
+// let firstVariable = "Hello World";
+//  firstVariable = 44;
+// let secondVariable = firstVariable;
+// secondVariable = "any string";
+// //the value of firstVariable is 44
+// let yourName = "fil";
+// console.log("Hello, my name is " + yourName + ".");
+
+//------------Booleans--------------//
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console.
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' == 'Name');
+  console.log(true != false);
+  console.log(false == false == false == false == false == false == true);
+  console.log(false == false);
+  console.log(e == 'Kevin');
+  console.log(a + b == c);
+  console.log(a * a == d);
+  console.log(48 == '48');
+
+
