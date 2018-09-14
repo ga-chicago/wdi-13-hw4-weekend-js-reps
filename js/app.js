aef1113fa7e935f0e25620a2df5356f349a21995
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-14 18:54:09 -0500

    the basics

--- js/app.js
+++ js/app.js
@@ -111,16 +111,24 @@ if (personAge >= 16) {
 
 
 
-//-----------------II LOOPS
+//-----------------II. LOOPS
 
 // Write a loop that will print out all the numbers from 0 to 10, inclusive.
 for(let i = 0; i <= 10; i++) {
-	console.log(i);
+	// console.log(i);
 }
 
 // Write a loop that will print out all the numbers from 10 up to and including 400.
 
+for(let i = 10; i <= 400; i++) {
+	// console.log(i);
+}
 
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000.
+
+for (let i = 12; i <= 4000; i += 3){
+	// console.log(i);
+}
 
 
 
