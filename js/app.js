c2c158c93a5c85682f41e2b26eda6c94338de4ee
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-14 16:14:34 -0500

    Q+A

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log("works");
+
+//------------Variables & Datatypes--------------//
+
+// How do we assign a value to a variable?
+// --we assign a value to a variable with const or let and using the = to assign a value to our variable
+// How do we change the value of a variable?
+// --we change the value of a variable by calling the variable 
+// How do we assign an existing variable to a new variable?
+//--by calling the variable and using the = sign to assign it a new value. then you call the variable and it should have the new value
+// Remind me, what are declare, assign, and define?
+//--declare is when you just declare a variables existence
+//-- asign is when you give the variable you declared a value with the = sign
+//--defining a value is when you declare a variable and assign it a value, then the var is defined.
+// What is pseudocoding and why should you do it?
+// --pseudocoding is where you write out steps on how you think every step of the program should go
+// What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+//--a majority of the time should be spent thinking and pseudocoding about how you think you're going to solve the problem. Solve the problem one step at a time, and check your work every step of the way.
+
