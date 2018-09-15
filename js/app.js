0495d675a0d0c582ae91c0092e4a8368882d30ab
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-14 19:01:53 -0500

    Get Even

--- js/app.js
+++ js/app.js
@@ -113,6 +113,10 @@ if (personAge >= 16) {
 
 //-----------------II. LOOPS
 
+
+// ------A. THE BASICS
+
+
 // Write a loop that will print out all the numbers from 0 to 10, inclusive.
 for(let i = 0; i <= 10; i++) {
 	// console.log(i);
@@ -130,7 +134,17 @@ for (let i = 12; i <= 4000; i += 3){
 	// console.log(i);
 }
 
+//------B. GET EVEN
 
+// Print out the even numbers that are within the range of 1 - 100.
+
+for (let i = 1; i <= 100; i++) {
+	if (i % 2 === 0) {
+		console.log(i, '<--Is an even number' );
+	}
+}
+
+// Adjust your code to add a message next to even numbers only that says: "<-- is an even number".
 
 
 
