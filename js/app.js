5618a217006f88a43c103f591055829960479ad7
LehmanBrother <chrislehman1991@gmail.com>
2018-09-16 14:01:46 -0500

    maxOfThree done

--- js/app.js
+++ js/app.js
@@ -239,6 +239,16 @@
 		}
 		return numArray;
 	}
+	//G. maxOfThree
+	const maxOfThree = (num1,num2,num3) => {
+		if(num1 >= num2 && num1 >= num3) {
+			return num1;
+		} else if(num2 >= num1 && num2 >= num3) {
+			return num2;
+		} else {
+			return num3;
+		}
+	}
 
 
 
