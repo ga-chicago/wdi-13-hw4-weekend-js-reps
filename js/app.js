a43346113aff29ad46498ed273cf9410d0ea0ec8
LehmanBrother <chrislehman1991@gmail.com>
2018-09-16 13:14:39 -0500

    isVowel done

--- js/app.js
+++ js/app.js
@@ -219,6 +219,14 @@
 		return number**3;
 	}
 	console.log(calculateCube(4));
+	//D. isVowel
+	const isVowel = (char) => {
+		if(char === "a" || char === "A" || char === "e" || char === "E" || char === "i" || char === "I" || char === "o" || char === "O" || char === "u" || char === "U") {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 
 
