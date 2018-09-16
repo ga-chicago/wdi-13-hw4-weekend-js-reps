4d10f48f363ca858f72ee0768d1eeefbaffe1324
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-15 22:48:09 -0500

    II.E - Multiples of 3 and 5 - Finished

--- javascript/app.js
+++ javascript/app.js
@@ -207,10 +207,20 @@ console.log(`You have $${bank_account2} in your bank account!`);
 
 
 // E. Multiples of 3 and 5
+
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000. If a previous question you've done has helpful bits of code in it that partially solves this problem, look back at them.
 // You just solved Project Euler problem 1!
 // Are you having dejà vu? This just in! From the "Read the entire problem before you start" dept: This problem was on a previous assignment. You may skip it if you've already done it, just include a comment saying that you've already done it. If you've now done the problem twice, perhaps next time you'll read the whole problem before starting it.
+let sum = 0;
+// I'm assuming "below" means "strictly below"
+for (i = 0; i < 1000; i++) {
+	if (i % 3 === 0 || i % 5 === 0) {
+		sum = sum + i;
+		// console.log(`sum: ${sum}, and i: ${i}`);
+	}
+}
+console.log(`${sum} <-- Sum of all the multiples of 3 or 5 below 1000`);
 // 🔴 Commit.
 
 
