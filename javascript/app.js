61585fe86ef5ff173f227344555422ba943cdc09
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-16 00:03:55 -0500

    III.Arrays and Control Flow - D.Change Values - Finished

--- javascript/app.js
+++ javascript/app.js
@@ -272,10 +272,16 @@ console.log(randomThings);
 
 
 // D. Change values
-// Given the following array const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+
+// Given the following array const 
+ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"];
 // What would you write to access the 3rd element of the array?
+ourClass[2];
 // Change the value of "Github" to "Octocat"
+ourClass[4] = "Octocat";
 // Add a new element, "Cloud City" to the array.
+ourClass.push("Cloud City");
+console.log(ourClass);
 // 🔴 Commit.
 
 
