87a188baa29bc4d418d37b3efc4a4691be9c8b40
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-16 18:42:12 -0500

    V.Objects - G.Functions Operating on Objects - Finished

--- javascript/app.js
+++ javascript/app.js
@@ -719,7 +719,16 @@ for (let j = 0; j < user.friend.purchased.length; j++) {
 // it should increment the user's age by 1
 // make the user's name uppercase
 // The function does not need a return statement, it will merely modify the user object.
+const updateUser = () => {
+	user.age++;
+	user.name = user.name.toUpperCase();
+}
 // Write a function oldAndLoud that performs the exact same tasks as updateUser, but instead of hard-coding it to only work on our user object, make it take a parameter person, and have it modify the object that is passed in as an argument when the function is called. Call your oldAndLoud function with user as the argument.
+const oldAndLoud = (person) => {
+	person.age++;
+	person.name = person.name.toUpperCase();
+}
+console.log(oldAndLoud(user));
 // 🔴 Commit.
 
 
