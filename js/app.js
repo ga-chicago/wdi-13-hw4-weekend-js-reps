ad4d512e998e637c9c4071c495aefb469d4f5210
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-16 21:13:20 -0500

    update the user

--- js/app.js
+++ js/app.js
@@ -581,12 +581,33 @@ return quotes[Math.floor(Math.random()*quotes.length)]
 }
 console.log(getRandomElement(quotes));
 
-//
+//-------------Objects
 
+//Let's set up an object data structure. Let's say we have a website that sells products, and we have a user of our website, and we want to store that user's data. The object data structure is a good way to organize the data from our user.
 
+//A. Make a user object
 
+//Create an object called user.
 
+const user = {
+  name: 'Josh',
+  email: 'Josh@gmail.com',
+  age: 27,
+  purchased: []
+}
+// Write in to the object the key-value pairs for name, email, age, and purchased. Set the value of purchased to an empty array []. Set the other values to whatever you would like.
 
+//B. Update the user
+
+//Our user has changed his or her email address. Without changing the original user object, update the email value to a new email address.
+
+user.email = 'newemail@gmail.com';
+console.log(user);
+
+//Our user has had a birthday! Without changing the original user object, increment the age value using the postfix operator. Hint: age++
+
+user.age++;
+console.log(user);
 
 
 
