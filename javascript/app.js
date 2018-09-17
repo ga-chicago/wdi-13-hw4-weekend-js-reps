fda5be7dd402ec47418e4b810ed934393e12ef65
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-17 10:07:11 -0500

    Objects Part G

--- javascript/app.js
+++ javascript/app.js
@@ -401,8 +401,21 @@ for(let i = 0; i < user.friend.purchased.length; i++) {
 	console.log(user.friend.purchased[i]);
 }
 
+// G.
+const updateUser = () => {
+	user.age ++;
+	user.name = user.name.toUpperCase();
+}
+
+updateUser();
 
 
+const oldAndLoud = (person) => {
+	person.age ++;
+	person.name = person.name.toUpperCase();
+}
+
+oldAndLoud(user);
 
 
 
