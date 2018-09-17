f8a6f211ef75067b7357339f3f1848e9ed3cb54e
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-16 20:18:14 -0500

    attempt at reverse word order

--- js/app.js
+++ js/app.js
@@ -370,6 +370,7 @@ const printLongestWord = (string) => {
 let words = printLongestWord(["destiny", "mountains", "snowboarding"]);
 console.log(words);
 
+//first attempt
 // const printLongestWord = (string) => {
 // 	//split string into an array of strings
 // 	let stringSplit = string.split(" ");
@@ -384,3 +385,19 @@ console.log(words);
 // 		return longestWord;
 // }
 // printLongestWord("Destiny is a poopest fart");
+
+//---------------Transmogify------------------//
+// Write a Javascript function called transmogrify. This function should accept three arguments, which you can assume will be numbers. Your function should return the "transmogrified" result.
+// The transmogrified result of three numbers is the product of the first two numbers, raised to the power of the third number.
+// For example, the transmogrified result of 5, 3, and 2 is (5 times 3) to the power of 2 is 225.
+
+//--------------reverseWordOrder---------------//
+
+const reverseWordOrder = (string) => {
+	let newString = " ";
+	for (let i = string.length -1 ; i >= 0; i--){
+		newString += string[i];
+	}
+	return newString;
+}
+console.log(reverseWordOrder("hey this is a string"));
